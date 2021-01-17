dd9f26f995b4d3ef96acc39fa880cb9349be2bc7
Roger King <rogr.king@gmail.com>
2021-01-17 14:34:53 -0500

    Initial Commit

---
+++ Shared/pqApp.swift
@@ -0,0 +1,17 @@
+//
+//  pqApp.swift
+//  Shared
+//
+//  Created by Roger King on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct pqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
